05f45db3d6749467a6389f1c2514427bc46301fc
Sam Saks-Fithian <samsaksfithian@gmail.com>
2019-07-31 15:36:27 -0700

    readline-sync mock

--- __tests__/lib.test.js
+++ __tests__/lib.test.js
@@ -1,4 +1,12 @@
-const { isWordSolved, stringify, createBlankWordArray } = require('../lib');
+const readlineSync = require('readline-sync');
+const {
+  isWordSolved,
+  stringify,
+  randomlySelectWord,
+  createBlankWordArray,
+  askForALetter,
+  print,
+} = require('../lib');
 
 // 'fit'/'it.focus' focuses test, 'xit'/'it.skip' skips test
 describe('stringify', () => {
@@ -56,5 +64,75 @@ describe('createBlankWordArray', () => {
     expect(createBlankWordArray('a word')).toHaveLength(0);
     expect(createBlankWordArray([1, 2, 3])).toHaveLength(0);
     expect(createBlankWordArray({ key: 'value' })).toHaveLength(0);
+  });
+});
+
+describe('isWordSolved', () => {
+  test('should return false if there is at least one underscore', () => {
+    const result = isWordSolved(['a', '_', 'c']);
+    expect(result).toBeFalsy();
+  });
+
+  test('should return true if there are no underscores', () => {
+    const result = isWordSolved(['a', 'b', 'c']);
+    expect(result).toBeTruthy();
+  });
+
+  test('should throw a TypeError if passed undefined input', () => {
+    expect(isWordSolved).toThrowError(TypeError);
+  });
+
+  test('should return false on empty input array', () => {
+    // TODO: implement this
+  });
+});
+
+describe('print', () => {
+  test('should log outpot to the console', () => {
+    // mock the console.log function
+    console.log = jest.fn();
+    print('Some input');
+    expect(console.log).toHaveBeenCalledTimes(1);
+    expect(console.log).toHaveBeenCalledWith('Some input');
+    // clears the mocked function data/calls/etc
+    console.log.mockClear();
+
+    // TODO: actually reset the function (console.log) back to original functionality
+  });
+});
+
+describe('randomlySelectWord', () => {
+  // Math.random = jest.fn(() => 0.5);
+  // Math.random = jest.fn().mockReturnValue(0.5);
+  Math.random = jest.fn();
+
+  test('should be able to return any word in the array', () => {
+    Math.random
+      .mockReturnValueOnce(0)
+      .mockReturnValueOnce(0.5)
+      .mockReturnValueOnce(0.99);
+    const arr = ['first', 'second', 'third'];
+    const result1 = randomlySelectWord(arr);
+    expect(result1).toBe('first');
+    const result2 = randomlySelectWord(arr);
+    expect(result2).toBe('second');
+    const result3 = randomlySelectWord(arr);
+    expect(result3).toBe('third');
+  });
+
+  // TODO: test for empty array
+  // TODO: test for undefined input
+});
+
+jest.mock('readline-sync');
+describe('askForALetter', () => {
+  test('should return the letter that the user put in', () => {
+    readlineSync.question
+      .mockReturnValueOnce('a')
+      .mockReturnValueOnce('z')
+      .mockReturnValueOnce('q');
+    expect(askForALetter()).toBe('a');
+    expect(askForALetter()).toBe('z');
+    expect(askForALetter()).toBe('q');
   });
 });
